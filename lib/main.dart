ce0d5386067493ca20c4b32879898940c14dc1aa
MuhammadZubair786 <mzhassan444@gmail.com>
2021-05-10 06:33:57 -0700

    Update Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,13 +17,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+       appBarTheme: AppBarTheme(
+     color: Colors.pink
+  )),
         home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(("First Flutter Assignemnet"),
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 30)),
+            
+      ),
       body: Center(
           child: Text(
-                      "Muhammad Zubair Minhas\n mzhassan444@gmail.com",
-                      style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+        "Muhammad Zubair Minhas\n mzhassan444@gmail.com",
+        style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
       )),
     ));
   }
 }
-
